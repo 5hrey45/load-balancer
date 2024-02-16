e56ac080fde54a0ffe67f69000899d4c25417450
Shreyas S S <shreyasnarve1611@gmail.com>
2024-02-17 00:34:39 +0530

    write round robin algo for rotation between servers

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
 	"net/url"
 )
 
+var (
+	servers = []string{
+		"http://127.0.0.1:5000",
+		"http://127.0.0.1:5001",
+		"http://127.0.0.1:5002",
+		"http://127.0.0.1:5003",
+		"http://127.0.0.1:5004",
+	}
+	lastServedIndex = 0
+)
+
 func main() {
 	http.HandleFunc("/", forwardRequest)
 	http.ListenAndServe(":8080", nil)
@@ -19,7 +30,8 @@ func forwardRequest(rw http.ResponseWriter, req *http.Request) {
 }
 
 func getServer() *url.URL {
-	url, err := url.Parse("http://127.0.0.1:5000")
+	url, err := url.Parse(servers[(lastServedIndex + 1) % 5])
+	lastServedIndex++
 	if err != nil {
 		log.Fatal(err)
 	}
